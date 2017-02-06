ae59abf8ddc7e8f0b90b0f5eb66fa56ef28143f3
Zhang Cheng <zhangcheng@cmss.chinamobile.com>
2017-02-06 18:17:47 +0800

    openmetric/compiler: add script to build go-carbon

--- build/build-compiler.sh
+++ build/build-compiler.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 set -e
 
+GOLANG_VERSION=1.6.4
+GOLANG_DOWNLOAD_URL=https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz
+GOLANG_DOWNLOAD_SHA256=b58bf5cede40b21812dfa031258db18fc39746cc0972bc26dae0393acc377aaf
+
 # carbon-c-relay build dependencies: git make gcc
 yum -y install git make gcc
+
+# install golang
+if [[ ! -e /build/golang.tar.gz ]]; then
+    echo "Downloading golang tarball: $GOLANG_DOWNLOAD_URL"
+    curl -fsSL "$GOLANG_DOWNLOAD_URL" -o /build/golang.tar.gz
+else
+    echo "Find local cache of golang tarball: /build/golang.tar.gz"
+fi
+echo "$GOLANG_DOWNLOAD_SHA256  /build/golang.tar.gz" | sha256sum -c -
+tar -C /usr/local -xzf /build/golang.tar.gz
+
+if [[ -n "$GOPATH" ]]; then
+    mkdir -p "$GOPATH/src" "$GOPATH/bin"
+fi
--- dockerfiles/compiler/compile.sh
+++ dockerfiles/compiler/compile.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
+TIME=$(date +%Y%m%d%H%M%S)
+
 CARBON_C_RELAY_REPO=https://github.com/grobian/carbon-c-relay.git
 CARBON_C_RELAY_REV=v2.6
-CARBON_C_RELAY_OUTPUT=/output/carbon-c-relay
+CARBON_C_RELAY_OUTPUT_DIR=/output/carbon-c-relay
+CARBON_C_RELAY_OUTPUT_FILE=relay-$CARBON_C_RELAY_REV-$TIME
+
+GO_CARBON_REPO=https://github.com/lomik/go-carbon.git
+GO_CARBON_REV=v0.9.0
+GO_CARBON_OUTPUT_DIR=/output/go-carbon
+GO_CARBON_OUTPUT_FILE=carbon-$GO_CARBON_REV-$TIME
 
 compile_carbon_c_relay() {
     echo "Compiling carbon-c-relay..."
-    TIME=$(date +%Y%m%d%H%M%S)
 
     git clone $CARBON_C_RELAY_REPO /build/carbon-c-relay
     cd /build/carbon-c-relay
     git checkout $CARBON_C_RELAY_REV
     make relay
-    mkdir -pv $CARBON_C_RELAY_OUTPUT
-    cp -v relay $CARBON_C_RELAY_OUTPUT/relay-$CARBON_C_RELAY_REV-$TIME
-    echo "Successfully compiled carbon-c-relay, output: $CARBON_C_RELAY_OUTPUT/relay-$CARBON_C_RELAY_REV-$TIME"
+    mkdir -pv $CARBON_C_RELAY_OUTPUT_DIR
+    cp -v relay $CARBON_C_RELAY_OUTPUT_DIR/$CARBON_C_RELAY_OUTPUT_FILE
+    echo "Successfully compiled carbon-c-relay, output: $CARBON_C_RELAY_OUTPUT_DIR/$CARBON_C_RELAY_OUTPUT_FILE"
+}
+
+compile_go_carbon() {
+    echo "Compiling go-carbon..."
+
+    git clone $GO_CARBON_REPO /build/go-carbon
+    cd /build/go-carbon
+    git checkout $GO_CARBON_REV
+    make submodules
+    make
+    mkdir -pv $GO_CARBON_OUTPUT_DIR
+    cp -v go-carbon $GO_CARBON_OUTPUT_DIR/$GO_CARBON_OUTPUT_FILE
+    echo "Successfully compiled go-carbon, output: $GO_CARBON_OUTPUT_DIR/$GO_CARBON_OUTPUT_FILE"
 }
 
 while [[ ${#} -gt 0 ]]; do
@@ -22,8 +42,12 @@ while [[ ${#} -gt 0 ]]; do
         carbon-c-relay)
             compile_carbon_c_relay
             ;;
+        go-carbon)
+            compile_go_carbon
+            ;;
         all)
             compile_carbon_c_relay
+            compile_go_carbon
             ;;
         *)
             echo "Unrecognized build target: $1, please check!"
